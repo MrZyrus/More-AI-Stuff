aac140e3cb39bccd8d22467bde8ec67823f4dc29
MrZyrus <kaiser705@gmail.com>
2017-02-11 23:02:09 -0300

    Add files via upload

--- AI2.py
+++ AI2.py
@@ -1,11 +1,11 @@
 #Need this for matrix operations
 import numpy as np
+import random
 
-def gradient_descent(x, y, n, m, theta, alpha, max_iter):
+def gradient_descent(x, y, n, m, alpha, max_iter):
     theta = np.ones(n) #Initialize theta as all 1
     for i in range(0, max_iter): #Using max number of iterations
-        h = np.dot(theta, x) #First, calculate the hypothesis
-        
+        h = np.dot(x, theta) #First, calculate the hypothesis
         #Then the gradient, this is the hardest step to understand
         #What happens here is we take the formula, it's the sum as follows
 
@@ -15,7 +15,26 @@ def gradient_descent(x, y, n, m, theta, alpha, max_iter):
 
         #But, we have numpy! and numpy.dot, so we just use that
         #and also we use all the h0s already calculated before
-
-        gradient = np.dot((h0 - y), x) / m 
+        gradient = np.dot((h - y), x) / m
         theta -= alpha * gradient #Lastly we update theta
     return theta
+
+def read_table(filename, n, m):
+    x = np.zeros(shape = (m, n))
+    y = np.zeros(shape = m)
+    f = open(filename, 'r')
+    i = 0
+    for line in f:
+        aux = line.split()
+        x[i][0] = 1
+        for j in range(1, n):
+            x[i][j] = aux[j]
+        y[i] = aux[n]
+        i += 1
+    return x, y
+
+n = 2
+m = 62
+x, y = read_table("test.txt", n, m)
+theta = gradient_descent(x, y, n, m, 0.1, 10)
+print(theta)
