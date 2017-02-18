691c3ab2dcd59301fbc82b2ff80ea5970650f485
MrZyrus <kaiser705@gmail.com>
2017-02-17 22:58:30 -0300

    Works now, fully updated

--- AI2.py
+++ AI2.py
@@ -1,18 +1,17 @@
 #Need this for matrix operations
 import numpy as np
-import math
+
 
 def gradient_descent(x, y, n, m, alpha, max_iter):
     theta = np.ones(n) #Initialize theta as all 1
     for i in range(0, max_iter): #Using max number of iterations
+        print("Iteration ", i, " Current theta ", theta)
         h = np.dot(x, theta) #First, calculate the hypothesis
         #Then the gradient, this is the hardest step to understand
         #What happens here is we take the formula, it's the sum as follows
-
         #sum = 0
         #for i in range(0, m): sum += (h0(x[i]) - y[i]) * x[i]
         #with x[0] = 1, then sum/m
-
         #But, we have numpy! and numpy.dot, so we just use that
         #and also we use all the h0s already calculated before
         gradient = np.dot((h - y), x) / m
@@ -43,11 +42,14 @@ def normalize(x, y, n):
 
 n = 2
 m = 62
-x, y = read_table('test.txt', n, m)
-print(x)
-print(y)
+x, y = read_table('data1.txt', n, m)
 x, y = normalize(x, y, n)
-print(x)
-print(y)
-theta = gradient_descent(x, y, n, m, 0.1, 1000)
+theta = gradient_descent(x, y, n, m, 0.01, 20000)
+print(theta)
+
+n = 4
+m = 20
+x, y = read_table('data2.txt', n, m)
+x, y = normalize(x, y, n)
+theta = gradient_descent(x, y, n, m, 0.01, 20000)
 print(theta)
